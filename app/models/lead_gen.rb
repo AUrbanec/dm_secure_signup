1b2d21fbbe9b9ee07ca199d201d1f2954169b19a
Prelang Builder <builder@prelang.com>
2017-03-25 17:39:49 +0000

    Defined validation for attribute 'zip_code' on model 'LeadGen'

--- app/models/lead_gen.rb
+++ app/models/lead_gen.rb
@@ -1,4 +1,5 @@
 class LeadGen < ActiveRecord::Base
   validates_formatting_of :email_address, using: :email
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :zip_code, using: :us_zip
 end
